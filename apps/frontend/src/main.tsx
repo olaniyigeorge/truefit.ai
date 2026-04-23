42ab4e789458c14662d4c927a6eb1abcda3573f4
Olaniyi George <olaniyigeorge77@gmail.com>
2026-04-23 15:09:22 +0100

    fix: polyfill crypto.randomUUID for non-HTTPS environments

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,4 +1,14 @@
-// import { StrictMode } from 'react'
+// crypto.randomUUID polyfill for non-HTTPS environments
+if (!crypto.randomUUID) {
+  (crypto as any).randomUUID = () => {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+      const r = Math.random() * 16 | 0
+      const v = c === 'x' ? r : (r & 0x3 | 0x8)
+      return v.toString(16)
+    })
+  }
+}
+
 import "@/helpers/api.interceptors"
 import { createRoot } from 'react-dom/client'
 import './index.css'
@@ -6,9 +16,7 @@ import App from './App.tsx'
 import AppProviders from './providers/AppProviders.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  // <StrictMode>
     <AppProviders>
         <App />
     </AppProviders>
-  // </StrictMode>,
 )
